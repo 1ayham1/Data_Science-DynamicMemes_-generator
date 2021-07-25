b3fbe3dec2bba27d1acc142ac0efd622647b0215
me <learn.ayham@zoho.com>
2021-07-25 16:43:34 +0200

    basic class implementation

---
+++ QuoteEngine/IngestorInterface.py
@@ -0,0 +1,17 @@
+""" """
+
+from abc import ABC, abstractmethod
+
+class IngestorInterface(ABC):
+    """ """
+    allowed_extensions = []
+
+    @classmethod
+    def can_ingest(cls,path):
+        ext = path.split('.')[-1]
+        return ext in cls.allowed_extensions
+    
+    @classmethod
+    @abstractmethod
+    def parse(cls,path: str):
+        pass
