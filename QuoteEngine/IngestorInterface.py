7962233c93c33d10f63b7d773ba21d61d2f4d97d
me <learn.ayham@zoho.com>
2021-07-26 09:55:23 +0200

    implementing the strategy interface

--- QuoteEngine/IngestorInterface.py
+++ QuoteEngine/IngestorInterface.py
@@ -1,9 +1,23 @@
-""" """
+""" Strategy to dynamically enable different file import behaviors
+
+Following open/closed principle (OCP), IngestorInterface (acting as
+Strategy Interface), encapsulates various import file behaviors which
+is resolved in real time. This enable flexible expansion to any new
+desired extension and decouples the desired behavior and the class
+that uses that behavior.
+
+The Strategy classes (CSVImporter, PDFImporter ...) encapsulate 
+specific implementation by using can_ingest() and overridding
+the abstract method parse(). 
+"""
 
 from abc import ABC, abstractmethod
 
+from typing import List
+from .QuoteModel import QuoteModel
 class IngestorInterface(ABC):
-    """ """
+    """Strategy for parsing various file formats"""
+
     allowed_extensions = []
 
     @classmethod
@@ -13,5 +27,5 @@ class IngestorInterface(ABC):
     
     @classmethod
     @abstractmethod
-    def parse(cls,path: str):
+    def parse(cls,path: str) -> List(QuoteModel):
         pass
