430df38c245f24ab06a79d542df2f34f2a64f22c
me <learn.ayham@zoho.com>
2021-07-26 12:18:09 +0200

    fixing style. PEP8

--- QuoteEngine/TXTImporter.py
+++ QuoteEngine/TXTImporter.py
@@ -5,6 +5,7 @@ from typing import List
 from .IngestorInterface import IngestorInterface
 from .QuoteModel import QuoteModel
 
+
 class TXTImporter(IngestorInterface):
     """Implement parse() to read .txt files through doc module
     and a composition to QuoteModel class that defines the used pattern"""
@@ -24,13 +25,13 @@ class TXTImporter(IngestorInterface):
             raise Exception('cannot ingest exception')
 
         quote_list = []
- 
+
         with open(path, 'r') as f:
             Lines = f.readlines()
 
             for line in Lines:
-                body,author = line.strip().split('-')
+                body, author = line.strip().split('-')
                 new_quote = QuoteModel(body, author)
                 quote_list.append(new_quote)
-   
+
         return quote_list
