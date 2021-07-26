0afe6239af5974e40f6a965042d9159da24f6b26
me <learn.ayham@zoho.com>
2021-07-26 13:05:19 +0200

    adding relative imports

---
+++ QuoteEngine/__init__.py
@@ -0,0 +1,5 @@
+from .Ingestor import Ingestor
+from .CSVImporter import CSVImporter
+from .PDFImporter import PDFImporter
+from .DocxImporter import DocxImporter
+from .TXTImporter import TXTImporter
