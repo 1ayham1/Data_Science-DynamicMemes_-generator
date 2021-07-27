51ee1b5763461ee3dcfed3781ed9d9935874dd15
me <learn.ayham@zoho.com>
2021-07-27 21:33:40 +0200

    adding types to init. fixing style: PEP 8

--- QuoteEngine/QuoteModel.py
+++ QuoteEngine/QuoteModel.py
@@ -1,11 +1,15 @@
 """encapsulate body and author"""
-class QuoteModel():
-    """ """
 
-    def __init__(self, body, author):
-        
+
+class QuoteModel:
+
+    def __init__(self, body: str, author: str) -> None:
+        """initialization"""
+
         self.body = body
         self.author = author
-    
+
     def __repr__(self):
+        """machine friendly object print"""
+
         return f'<{self.body}, {self.author}'
